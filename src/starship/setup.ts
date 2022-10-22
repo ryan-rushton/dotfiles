2ae0de21b2a558f4302c36dd1fc375056087907b
Ryan <ryan.rushton79@gmail.com>
2022-10-23 09:08:24 +1000

    Format

--- src/starship/setup.ts
+++ src/starship/setup.ts
@@ -1,19 +1,19 @@
-import { homedir } from 'os'
-import { join } from 'path'
-import { createSymlink, mkdir } from '../utils/utils'
+import { homedir } from 'os';
+import { join } from 'path';
+import { createSymlink, mkdir } from '../utils/utils';
 
-const CONFIG_FILE = 'starship.toml'
+const CONFIG_FILE = 'starship.toml';
 
-async function setup () {
-  console.info('Setting up starship')
-  const home = homedir()
-  const dotConfig = join(home, '.config')
-  await mkdir(dotConfig)
+async function setup() {
+  console.info('Setting up starship');
+  const home = homedir();
+  const dotConfig = join(home, '.config');
+  await mkdir(dotConfig);
 
-  const target = join(dotConfig, CONFIG_FILE)
-  const config = join(__dirname, CONFIG_FILE)
+  const target = join(dotConfig, CONFIG_FILE);
+  const config = join(__dirname, CONFIG_FILE);
 
-  await createSymlink(config, target)
+  await createSymlink(config, target);
 }
 
-setup()
+setup();
